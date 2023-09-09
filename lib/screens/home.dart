239d83ae85393e9d20325ab59554a0c990dfac3f
Andrea Riccardi <andrea.riccardi03@gmail.com>
2023-09-09 19:06:55 +0200

    backup before repair my mac

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -21,7 +21,7 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        decoration: BoxDecoration(
+        decoration: const BoxDecoration(
           gradient: LinearGradient(
             begin: Alignment.topCenter,
             end: Alignment.bottomCenter,
